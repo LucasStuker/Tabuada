ebe78b61f19a41f6d44e310c69521f1540e67415
LucasStuker <lucasstuker@gmail.com>
2023-03-22 14:40:55 -0300

    finalizado

--- variaveis/aula/Tabuada/tabuda_script.js
+++ variaveis/aula/Tabuada/tabuda_script.js
@@ -1,5 +1,20 @@
 function tabuada (){
     var num = window.document.getElementById('num')
     var end = window.document.getElementById('end')
-    window.alert('funcionei')
-}
+    if(num.value.length == 0 || end.value.length == 0){
+        window.alert("[ERRO] confira os dados")
+    } else {
+        var n = Number(num.value)
+        var e = Number(end.value)
+        var x = 0
+        var r = ''
+        var res = window.document.getElementById('resposta')
+        r = n*e
+        res.innerHTML = `A tabudada do ${n} é: <br>`
+        do {
+            r = n*x
+            res.innerHTML += `${x} x ${n} = ${r} <br>`
+            x++
+        } while (x<= e)
+    
+    } }
